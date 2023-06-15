ad30a097f9de5e55216a0efaa1c2415a51b96ab6
Jennifer Tam <jenniferwtam@gmail.com>
2023-06-14 19:50:07 -0700

    ChatEntry/App compon render sender/body/timestamp

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
 
 const App = () => {
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>iMessage</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+      <ChatEntry
+          sender="Vladimir"
+          body="why are you arguing with me"
+          timeStamp="2018-05-29T22:49:06+00:00"
+        ></ChatEntry>
+        {/* Wave 02: Render ChatLog component */}
       </main>
     </div>
   );
 };
 
 export default App;
+
+
+
+
+
+
+
+
+
+
