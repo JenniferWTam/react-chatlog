4da6ede139c3374889bacd5df4d5609d19813672
Jennifer Tam <jenniferwtam@gmail.com>
2023-06-21 10:13:20 -0700

    reverted my file back to before i messed it up

--- src/components/ColorChoice.js
+++ src/components/ColorChoice.js
@@ -42,3 +42,88 @@ const ColorChoice = ({ setColorCallback }) => {
 };
 
 export default ColorChoice;
+
+
+
+// TRIED THIS - DIDNT WORK
+
+// import React from 'react';
+
+// const ColorChoice = ({ setColorCallback }) => {
+//     const handleColorSelection = (sender, color) => {
+//       setColorCallback(sender, color);
+//     };
+
+//   return (
+//     <div className="color-choice">
+//       {/* LOCAL */}
+//       <div>
+//         <button onClick={() => handleColorSelection('local', 'red')}>
+//           <span role="img" aria-label="Red">
+//             🔴
+//           </span>
+//         </button>
+//          <button onClick={() => handleColorSelection('local', 'orange')}>
+//          <span role="img" aria-label="Orange">
+//            🟠
+//          </span>
+//        </button>
+//        <button onClick={() => handleColorSelection('local', 'yellow')}>
+//          <span role="img" aria-label="Yellow">
+//            🟡
+//          </span>
+//        </button>
+//        <button onClick={() => handleColorSelection('local', 'green')}>
+//          <span role="img" aria-label="Green">
+//            🟢
+//          </span>
+//        </button>
+//        <button onClick={() => handleColorSelection('local', 'blue')}>
+//          <span role="img" aria-label="Blue">
+//            🔵
+//          </span>
+//        </button>
+//        <button onClick={() => handleColorSelection('local', 'purple')}>
+//          <span role="img" aria-label="Purple">
+//            🟣
+//          </span>
+//        </button>
+//        </div>
+// {/* REMOTE */}
+//       <div>
+//         <button onClick={() => handleColorSelection('remote', 'red')}>
+//           <span role="img" aria-label="Red">
+//             🔴
+//           </span>
+//         </button>
+//         <button onClick={() => handleColorSelection('remote', 'orange')}>
+//         <span role="img" aria-label="Orange">
+//           🟠
+//         </span>
+//       </button>
+//       <button onClick={() => handleColorSelection('remote', 'yellow')}>
+//         <span role="img" aria-label="Yellow">
+//           🟡
+//         </span>
+//       </button>
+//       <button onClick={() => handleColorSelection('remote', 'green')}>
+//         <span role="img" aria-label="Green">
+//           🟢
+//         </span>
+//       </button>
+//       <button onClick={() => handleColorSelection('remote', 'blue')}>
+//         <span role="img" aria-label="Blue">
+//           🔵
+//         </span>
+//       </button>
+//       <button onClick={() => handleColorSelection('remote', 'purple')}>
+//         <span role="img" aria-label="Purple">
+//           🟣
+//         </span>
+//       </button>
+//     </div>
+//     </div>
+//   );
+// };
+
+//     export default ColorChoice;
